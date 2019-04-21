6025a29d7e52aa0ddb6a78b10976a12a818d9e34
Dominic Pearson <dsp@technoanimal.net>
2019-04-21 18:36:19 +0200

    Let binding macro for frame operations.

--- src/phlegyas/util.clj
+++ src/phlegyas/util.clj
@@ -1,6 +1,36 @@
 (ns phlegyas.util
   (:import java.nio.ByteBuffer))
 
+(defmacro with-frame-bindings
+  [data body]
+  `(let [frame# ~data
+         ~'frame-type (:frame frame#)
+         ~'frame-tag (:tag frame#)
+         ~'frame-qid-type (:qid-type frame#)
+         ~'frame-qid-vers (:qid-vers frame#)
+         ~'frame-qid-path (:qid-path frame#)
+         ~'frame-nwqids (:nwqids frame#)
+         ~'frame-iounit (:iounit frame#)
+         ~'frame-count (:count frame#)
+         ~'frame-ssize (:ssize frame#)
+         ~'frame-size (:size frame#)
+         ~'frame-type (:type frame#)
+         ~'frame-mode (:mode frame#)
+         ~'frame-atime (:atime frame#)
+         ~'frame-mtime (:mtime frame#)
+         ~'frame-length (:length frame#)
+         ~'frame-name (:name frame#)
+         ~'frame-uid (:uid frame#)
+         ~'frame-gid (:gid frame#)
+         ~'frame-muid (:muid frame#)
+         ~'frame-data (:data frame#)
+         ~'frame-offset (:offset frame#)
+         ~'frame-fid (:fid frame#)
+         ~'frame-ename (:ename frame#)
+         ~'frame-version (:version frame#)
+         ~'frame-msize (:msizez frame#)]
+       ~@body))
+
 (defn wrap-buffer
   "Wraps a byte-array in a Java ByteBuffer, using little-endian
   byte order as required by the 9P2000 protocol."
