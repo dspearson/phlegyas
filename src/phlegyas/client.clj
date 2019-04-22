7c37360b1658b5e64c93d7edb573e427fa22ffbf
Dominic Pearson <dsp@technoanimal.net>
2019-04-22 11:03:50 +0200

    Make functions more transparent, don't copy OO.

--- src/phlegyas/client.clj
+++ src/phlegyas/client.clj
@@ -10,64 +10,60 @@
 
 ;; an example implementation of a client.
 
-(defn next-available-value
-  "Function for use with atomic swap, to find next value not
-  belonging to the set."
+(defn next-available
   [p]
   (loop [i 0]
     (if (not (contains? p i))
       i
       (recur (inc i)))))
 
-(defn numeric-pool
-  "Returns a function that acts as an allocator/deallocator.
-  Call with no arguments to return the next available number, and
-  call with `:clunk n` or `:clunk #{n m n}`, to remove values from
-  use."
-  []
-  (let [vals (atom #{})]
-    (fn
-      [& {:keys [clunk] :or {clunk nil}}]
-      (if clunk
-        (swap! vals (fn [x] (disj x clunk)))
-        (let [[old new] (swap-vals! vals (fn [x] (conj x (next-available-value x))))]
-          (first (sets/difference new old)))))))
+(defn alloc-val
+  [pool]
+  (let [[old new] (swap-vals! pool (fn [x] (conj x (next-available x))))]
+    (first (sets/difference new old))))
+
+(defn clunk-val
+  [pool value]
+  (swap! pool (fn [x] (disj x value))))
 
 (defn tag-and-assemble
   [x in-flight tagpool]
   (let [frame (:frame x)
         response (:response x)
-        tag (tagpool)]
+        tag (alloc-val tagpool)]
     (swap! in-flight (fn [x] (assoc x (keywordize tag) response)))
     (-> frame (assoc :tag tag) assemble-packet)))
 
-(defn transactional-actor
+(defn transact
   "Returns a function that takes in frames and returns deferreds
   representing the response over the network. Frames do not
   require tags."
-  [x]
-  (fn [y]
-    (let [response (d/deferred)]
-      (s/put! x {:response response :frame y})
-      response)))
+  [connection transaction]
+  (let [response (d/deferred)]
+    (s/put! (:outgoing-frame-stream connection) {:response response :frame transaction})
+    response))
 
 (defn client!
   "This example client returns a function that will take in frames (without tags)
   and will automatically handle matching responses for you. Calls to the returned
   function are a deferred that is delivered when the response is received."
   [in]
-  (let [tagpool (numeric-pool)
+  (let [tagpool (atom #{})
         incoming-frame-stream (s/stream)
         outgoing-frame-stream (s/stream)
         in-flight (atom {})
-        _ (frame-assembler in incoming-frame-stream)]
+        frame-assembler-thread (frame-assembler in incoming-frame-stream)]
     (s/consume (fn [x] (let [tag (:tag x)
                             response ((keywordize tag) @in-flight)]
                         (swap! in-flight dissoc tag)
-                        (tagpool :clunk tag)
+                        (clunk-val tagpool tag)
                         (d/success! response x))) incoming-frame-stream)
     (s/connect-via outgoing-frame-stream #(s/put! in (tag-and-assemble % in-flight tagpool)) in)
-    (transactional-actor outgoing-frame-stream)))
+    {:tag-pool tagpool
+     :incoming-frame-stream incoming-frame-stream
+     :outgoing-frame-stream outgoing-frame-stream
+     :in-flight in-flight
+     :frame-assembler frame-assembler-thread}))
 
 (defn connect
   [host port]
@@ -82,26 +78,26 @@
   ([x username fs-name]
    (let [state (:state x)
          fid-pool (:fid-pool @state)
-         attach-fid (fid-pool)
+         attach-fid (alloc-val fid-pool)
          connection (:connection x)
-         response @(connection {:frame :Tattach :uname username :aname fs-name :fid attach-fid :afid nofid})]
+         response @(transact connection {:frame :Tattach :uname username :aname fs-name :fid attach-fid :afid nofid})]
      (if (= (:frame response) :Rattach)
        (do
          (swap! state (fn [x] (assoc x :fids (assoc (:fids x) attach-fid {:name "/" :filesystem fs-name}))))
          attach-fid)
        (do
-         (fid-pool :clunk attach-fid)
+         (clunk-val fid-pool attach-fid)
          false)))))
 
 (defn negotiate-version
   [x]
   (let [connection (:connection x)
         state (:state x)
-        response @(connection {:frame :Tversion :msize max-message-size :version protocol-version})]
+        response @(transact connection {:frame :Tversion :msize max-message-size :version protocol-version})]
     (reset! state {}) ;; a version request restarts a connection.
     (if (= (:frame response) :Rversion)
       (do
-        (swap! state assoc :msize (:msize response) :version (:version response) :fid-pool (numeric-pool))
+        (swap! state assoc :msize (:msize response) :version (:version response) :fid-pool (atom #{}))
         (:version response))
       false)))
 
@@ -110,14 +106,14 @@
   (let [connection (:connection x)
         state (:state x)
         fid-pool (:fid-pool @state)
-        requested-fid (fid-pool)
-        response @(connection {:frame :Twalk :fid fs-handle :newfid requested-fid :wnames []})]
+        requested-fid (alloc-val fid-pool)
+        response @(transact connection {:frame :Twalk :fid fs-handle :newfid requested-fid :wnames []})]
     (if (= (:frame response) :Rwalk)
       (do
         (swap! state (fn [x] (assoc x :fids (assoc (:fids x) requested-fid (get (:fids x) fs-handle)))))
         requested-fid)
       (do
-        (fid-pool :clunk requested-fid)
+        (clunk-val fid-pool requested-fid)
         false))))
 
 (defn walk-fid
@@ -125,8 +121,8 @@
   (let [connection (:connection x)
         state (:state x)
         fid-pool (:fid-pool @state)
-        requested-fid (fid-pool)
-        response @(connection {:frame :Twalk :fid fs-handle :newfid requested-fid :wnames paths})]
+        requested-fid (alloc-val fid-pool)
+        response @(transact connection {:frame :Twalk :fid fs-handle :newfid requested-fid :wnames paths})]
     (if (and (= (:frame response) :Rwalk) (= (count (:nwqids response)) (count paths)))
       (do
         (swap! state (fn [y] (assoc y :fids (assoc (:fids y) requested-fid
@@ -137,26 +133,26 @@
                                                    :filesystem (:filesystem (get (:fids @state) fs-handle))}))))
         requested-fid)
       (do
-        (fid-pool :clunk requested-fid)
+        (clunk-val fid-pool requested-fid)
         false))))
 
 (defn open-fid
   [x fid iomode]
   (let [connection (:connection x)
-        response @(connection {:frame :Topen :fid fid :iomode iomode})]
+        response @(transact connection {:frame :Topen :fid fid :iomode iomode})]
     (:iounit response)))
 
 (defn read-fid-partial
   [x fid offset iounit]
   (let [connection (:connection x)
-        response @(connection {:frame :Tread :fid fid :offset offset :count iounit})]
+        response @(transact connection {:frame :Tread :fid fid :offset offset :count iounit})]
     (:data response)))
 
 (defn clunk-fid
   [x fid]
   (let [connection (:connection x)
         state (:state x)
-        response @(connection {:frame :Tclunk :fid fid})]
+        response @(transact connection {:frame :Tclunk :fid fid})]
     (if (= (:frame response) :Rclunk)
       (do
         (swap! state (fn [y] (assoc y :fids (dissoc (:fids y) fid))))
@@ -186,7 +182,7 @@
         (let [y (into {} (for [elem layout] {elem ((elem buffer-functions) buf)}))]
           (recur (assoc stats (:qid-path y) y)))))))
 
-(defn open-and-read
+(defn open-and-read-fid
   [x fid]
   (let [fid-clone (clone-fid x fid)
         io-unit (open-fid x fid-clone 0)
