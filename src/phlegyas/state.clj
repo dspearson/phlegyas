e8d035074882b540dbf8af04bc46b68e33bec2ad
Dominic Pearson <dsp@technoanimal.net>
2019-04-23 13:38:53 +0200

    Undo experiments with macros.

--- src/phlegyas/state.clj
+++ src/phlegyas/state.clj
@@ -23,24 +23,9 @@
   [data]
   `(let [state-update# (:update ~data)
          reply-typ# ((keywordize (+ 1 ((:frame ~'frame) ~'frame-byte))) ~'reverse-frame-byte)
-         frame-update# (assoc (:reply ~data) :frame reply-typ#)]
-     (if state-update#
-       (cond
-         (clojure.test/function? state-update#)
-         (swap! ~'state state-update#)
-
-         (and (list? state-update#) (list? (first state-update#)))
-         (swap! ~'state (fn [x#] (loop [applicators# state-update#
-                                       s# x#]
-                                  (if (nil? (first applicators#))
-                                    s#
-                                    (recur (rest applicators#)
-                                           (apply (ns-resolve ~*ns* (ffirst applicators#))
-                                                  (cons s# (map (fn [x#] (ns-resolve ~*ns* x#)) (rest (first applicators#))))))))))
-
-         :else
-         (swap! ~'state (fn [x#] (apply (ns-resolve ~*ns* (first state-update#))
-                                       (cons x# (map (fn [x#] (ns-resolve ~*ns* x#)) (rest state-update#))))))))
+         frame-update# (assoc (:reply ~data) :frame reply-typ#)
+         _# (if state-update#
+              (swap! ~'state state-update#))]
      (into ~'frame frame-update#)))
 
 (defn-frame-binding Tversion
@@ -49,7 +34,7 @@
     (not (string/starts-with? frame-version protocol-version)) (state! {:reply {:version "unknown"}})
     (<= frame-msize max-message-size) (state! {:update (fn [x] (assoc x :msize frame-msize))
                                                :reply {:version protocol-version}})
-    :else (state! {:update (assoc :msize max-message-size)
+    :else (state! {:update (fn [x] (assoc x :msize max-message-size))
                    :reply {:version protocol-version
                            :msize max-message-size}})))
 
@@ -62,10 +47,11 @@
   (let [root-fs ((:root-filesystem current-state))
         root-fs-id (:id root-fs)
         root-path (:root-path root-fs)]
-    (state! {:update '((add-fs root-fs)
-                       (add-fid frame-fid frame-tag)
-                       (add-mapping frame-fid root-fs-id root-path)
-                       (add-role root-fs-id frame-uname frame-uname))
+    (state! {:update (fn [x] (-> x
+                                (add-fs root-fs)
+                                (add-fid frame-fid frame-tag)
+                                (add-mapping frame-fid root-fs-id root-path)
+                                (add-role root-fs-id frame-uname frame-uname)))
              :reply (path->qid root-fs root-path)})))
 
 (defn-frame-binding Tflush
@@ -75,15 +61,17 @@
 (defn-frame-binding Twalk
   [frame connection]
   (if (= (count frame-wnames) 0)
-    (state! {:update '((add-fid frame-newfid frame-tag)
-                       (add-mapping frame-newfid fs-name path))
+    (state! {:update (fn [x] (-> x
+                                (add-fid frame-newfid frame-tag)
+                                (add-mapping frame-newfid fs-name path)))
              :reply {:nwqids []}})
     (let [wname-paths (walk-path fs path frame-wnames)
           qids (for [p wname-paths] (stat->qid (path->stat fs p)))]
       (if (< (count wname-paths) (count frame-wnames))
         (state! {:reply {:nwqids qids}})
-        (state! {:update '((add-fid frame-newfid frame-tag)
-                           (add-mapping frame-newfid fs-name (last wname-paths)))
+        (state! {:update (fn [x] (-> x
+                                    (add-fid frame-newfid frame-tag)
+                                    (add-mapping frame-newfid fs-name (last wname-paths))))
                  :reply {:nwqids qids}})))))
 
 (defn-frame-binding Topen
@@ -92,7 +80,7 @@
         stat (path->stat fs path)]
     (if (not (permission-check stat role :oread))
       (error! "no read permission")
-      (state! {:update '(update-mapping frame-fid {:offset 0})
+      (state! {:update (fn [x] (update-mapping x frame-fid {:offset 0}))
                :reply {:iounit (iounit!)
                        :qid-type (:qid-type stat)
                        :qid-vers (:qid-vers stat)
@@ -104,10 +92,12 @@
         parent-stat (fid->stat current-state frame-fid)
         parent-path (:parent parent-stat)
         file-path (next-available-path fs)]
-    (state! {:update '((assoc-in [:fs-map fs-name :files file-path]
-                                 (into new-stat {:qid-path file-path :parent parent-path}))
-                       (update-in [:fs-map fs-name :files parent-path]
-                                  (fn [x] (assoc x :children (conj (:children x) file-path)))))
+    (state! {:update (fn [x]
+                       (-> x
+                           (assoc-in [:fs-map fs-name :files file-path]
+                                     (into new-stat {:qid-path file-path :parent parent-path}))
+                           (update-in [:fs-map fs-name :files parent-path]
+                                      (fn [y] (assoc y :children (conj (:children y) file-path))))))
              :reply {:qid-type (:qid-type new-stat)
                      :qid-vers (:qid-vers new-stat)
                      :qid-path file-path
@@ -138,8 +128,8 @@
                    [dir-data paths-remaining] (directory-reader (into [] (for [x dirpaths] (path->stat fs x))) frame-count)
                    delivered-byte-count (count dir-data)]
 
-               (state! {:update '(update-mapping frame-fid {:offset (+ frame-offset (count dir-data))
-                                                            :paths-remaining paths-remaining})
+               (state! {:update (fn [x] (update-mapping x frame-fid {:offset (+ frame-offset (count dir-data))
+                                                                    :paths-remaining paths-remaining}))
                         :reply {:data dir-data}})))
 
       :file (if (and (not= 0 (:length stat)) (>= frame-offset (:length stat))) ; if offset >= length, it means that we are
@@ -161,8 +151,8 @@
 
 (defn-frame-binding Tclunk
   [frame connection]
-  (state! {:update (fn [x] (into {:fids (dissoc (:fids x) frame-fid)
-                                 :mapping (dissoc (:mapping x) frame-fid)}))}))
+  (state! {:update (fn [x] (-> (into x {:fids (dissoc (:fids x) frame-fid)
+                                       :mapping (dissoc (:mapping x) frame-fid)})))}))
 
 (defn-frame-binding Tremove
   [frame connection]
@@ -192,7 +182,6 @@
   (log/debug "in:" frame)
   (conj-val (:in-flight-requests connection) (:tag frame))
   (let [reply (((:frame frame) state-handlers) frame connection)]
-    (log/debug "state:" @(:state connection))
     (log/debug "out:" reply)
     (s/put! out reply)
     (disj-val (:in-flight-requests connection) (:tag frame))))
