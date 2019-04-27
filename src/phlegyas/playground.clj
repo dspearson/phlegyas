a72e868d531be97f1fadbfdf8b6e471fcffa7f8d
Dominic Pearson <dsp@technoanimal.net>
2019-04-27 18:32:14 +0200

    Profiling namespace.

---
+++ src/phlegyas/playground.clj
@@ -0,0 +1,45 @@
+(ns phlegyas.playground
+  (:require [phlegyas.frames :refer :all]
+            [phlegyas.state :refer :all]
+            [phlegyas.vfs :refer :all]
+            [phlegyas.util :refer :all]
+            [phlegyas.client :refer :all]
+            [clojure.core.async :as async]
+            [manifold.stream :as s]
+            [manifold.deferred :as d]
+            [aleph.tcp :as tcp]
+            [taoensso.timbre :as log])
+  (:gen-class))
+
+;; useful for profiling.
+
+(defn test-connection
+  []
+  (let [state (atom {:root-filesystem #'example-filesystem!})
+        incoming-frame-stream (s/stream)
+        outgoing-frame-stream (s/stream)
+        in-flight-requests (atom #{})
+        ;; frame-assembler-thread (frame-assembler in incoming-frame-stream)
+        connection {:state state
+                    :incoming-frame-stream incoming-frame-stream
+                    :outgoing-frame-stream outgoing-frame-stream
+                    :in-flight-requests in-flight-requests
+                    :in-stream nil
+                    :out-stream nil
+                    :frame-assembler-thread nil}]
+    connection))
+
+(log/set-level! :trace)
+
+(def conn (test-connection))
+
+(Tversion {:frame :Tversion :msize 8192 :tag 0 :version "9P2000"} conn)
+(Tattach {:frame :Tattach :tag 0 :afid 0 :fid 0 :uname "dsp" :aname "dsp"} conn)
+
+(time
+ (for [x (range 1 501)]
+   (do
+     (Twalk {:frame :Twalk :tag 0 :fid 0 :newfid x :wnames []} conn)
+     (Twalk {:frame :Twalk :tag 0 :fid x :newfid x :wnames [(str x)]} conn)
+     (Tcreate {:frame :Tcreate :tag 0 :fid x :name (str x) :perm 0755 :iomode 1} conn)
+     (Tclunk {:frame :Tclunk :tag 0 :fid x} conn))))
