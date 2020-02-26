8a36116769e7bc631deb0f4136466835de1e77cf
Dominic Pearson <dsp@technoanimal.net>
2020-02-26 16:49:32 +0100

    Support --port / -p cli opts for server.

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [aleph "0.4.6"]
                  [com.taoensso/timbre "4.10.0"]
                  [buddy/buddy-core "1.6.0"]
-                 [org.clojure/core.incubator "0.1.4"]]
+                 [org.clojure/core.incubator "0.1.4"]
+                 [org.clojure/tools.cli "1.0.194"]]
   :plugins [[cider/cider-nrepl "0.24.0"]
             [jonase/eastwood "0.3.7"]]
   :main ^:skip-aot phlegyas.core
--- src/phlegyas/core.clj
+++ src/phlegyas/core.clj
@@ -1,7 +1,15 @@
 (ns phlegyas.core
-  (:require [phlegyas.server :refer :all])
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [phlegyas.server :refer :all])
   (:gen-class))
+
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+    :default 10001
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]])
 
 (defn -main
   [& args]
-  (start-server))
+  (let [opts (parse-opts args cli-options)]
+    (start-server (get-in opts [:options :port]))))
