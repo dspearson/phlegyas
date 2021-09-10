6caf362cc981525fd9c275ff621164c467db4cca
Dominic Pearson <dsp@technoanimal.net>
2021-09-10 13:21:34 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/dspearson/phlegyas"
   :license {:name "ISC Licence"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/core.async "1.3.610"]
+                 [org.clojure/core.async "1.3.618"]
                  [primitive-math "0.1.6"]
-                 [manifold "0.1.8"]
+                 [manifold "0.1.9"]
                  [aleph "0.4.6"]
                  [com.taoensso/timbre "5.1.2"]
                  [org.clojure/core.incubator "0.1.4"]
