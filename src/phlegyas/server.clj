91d0d55ed390caee3487b294a9feec0a10aa062c
Dominic Pearson <dsp@technoanimal.net>
2022-04-08 18:15:19 +0200

    Some extensions to the tcp routing mechanism.
    This is some initial work to see if there is some nice developer-friendly
    interface that can be created to allow greater ergonomics for custom handler
    implementations. Right now almost the whole server namespace, along with the
    state handler, etc., need to be essentially duplicated in real-world
    implementations. The frame handlers, assembly/disassembly, etc. are the only
    real static out-of-the-box drop-in things that I am happy with. Herein lies the
    beginning of making this an easier library for direct integration into other
    upstream projects. (Admittedly, primarily my own for starters.)

--- src/phlegyas/server.clj
+++ src/phlegyas/server.clj
@@ -6,13 +6,15 @@
             [aleph.tcp :as tcp]))
 
 (defn server!
-  [in out & {:keys [root-filesystem-constructor]}]
+  [in out & {:keys [root-filesystem-constructor handler]}]
   (let [state                  (atom {:root-filesystem (or root-filesystem-constructor #'example-filesystem!)})
+        handler                (or handler state-handler)
         incoming-frame-stream  (s/stream)
         outgoing-frame-stream  (s/stream)
         in-flight-requests     (atom #{})
         frame-assembler-thread (frame-assembler in incoming-frame-stream)
         connection             {:state                  state
+                                :handler handler
                                 :incoming-frame-stream  incoming-frame-stream
                                 :outgoing-frame-stream  outgoing-frame-stream
                                 :in-flight-requests     in-flight-requests
@@ -20,8 +22,17 @@
                                 :out-stream             out
                                 :frame-assembler-thread frame-assembler-thread}]
     (s/connect-via outgoing-frame-stream #(s/put! out (assemble-packet %)) out)
-    (consume incoming-frame-stream outgoing-frame-stream connection #'state-handler)
+    (consume incoming-frame-stream outgoing-frame-stream connection state-handler)
     connection))
+
+(defn tcp-route-with-custom-handlers
+  [handler connection-atom]
+  (fn [s _]
+    (let [in  (s/stream)
+          out (s/stream)
+          _   (reset! connection-atom (server! in out :handler handler))]
+      (s/connect s in)
+      (s/connect out s))))
 
 (defn tcp-route
   [s _]
@@ -37,4 +48,8 @@
   ([port]
    (start-server port true))
   ([port join?]
-   (tcp/start-server tcp-route {:port port :join? join?})))
+   (start-server port join? (atom nil)))
+  ([port join? connection]
+   (let [router (tcp-route-with-custom-handlers state-handler connection)
+         server (tcp/start-server tcp-route {:port port :join? join?})]
+     server)))
